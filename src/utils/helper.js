5e1a9d4e5d61eaec919b9f9b03ff6fb2417e6da3
Anshuman Ranjan <anshumanranjan1998@gmail.com>
2024-05-17 12:05:24 +0000

    Updated Backend baseURL

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,4 +1,5 @@
 export const server = {
     // baseURL: "https://automatic-parakeet-w9r66557r4q3vv7g-5000.app.github.dev"
-    baseURL: "http://localhost:5000"
+    // baseURL: "http://localhost:5000"
+    baseURL: "https://blog-fullstack-app-reactjs-and-nestjs.onrender.com"
 }
